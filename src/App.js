7ba75a0a18018a1f6b006929dcc9431dc2137bb9
Chuck Gonzalez <chuck.gonzalez02@gmail.com>
2019-08-19 21:55:49 -0500

    Styles implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Suspense } from 'react';
 import interfaces from './interfaces';
+import styles from './generated/styles.js';
 import manifest from './generated/manifest.json';
 import StoreInterface from 'corex-store-interface';
 
 const App = () => {
   return (
     <StoreInterface
+      styles={styles}
       manifest={manifest}
       render={(props) => {
         return (
