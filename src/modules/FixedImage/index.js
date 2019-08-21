cab7176c436f3be13c435dbaeadc3b97ba6e0211
Oscar Cárdenas <oscar.cardenas@elektra.com.mx>
2019-08-21 00:04:03 -0500

    Heade dinamico usuando tachyons

--- src/modules/FixedImage/index.js
+++ src/modules/FixedImage/index.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import styles from './index.module.sass';
 import Button from '../Button';
 
-const FixedImage = props => {
+const FixedImage = (props) => {
   const device = props.device;
   const screenSize = device.screenSize;
   const cube = props.cube;
@@ -12,23 +12,17 @@ const FixedImage = props => {
   if (!blocks || !blocks.length) return null;
 
   const [block] = blocks;
-  const [
-    image,
-    title,
-    text,
-    textColor,
-    buttonLink,
-    callToAction
-  ] = block.props;
+  const [image, title, text, textColor, buttonLink, callToAction] = block.props;
 
   return (
     <section className={styles[screenSize]}>
       <div className={styles.container}>
         <div
           className={styles.textContent}
-          style={{ backgroundImage: `url(${image.value})` }}
-        >
-          <div className={styles.textContainer} style={{ color: textColor.value }}>
+          style={{ backgroundImage: `url(${image.value})` }}>
+          <div
+            className={styles.textContainer}
+            style={{ color: textColor.value }}>
             <h2 className={styles.title}>{title.value}</h2>
             <h3 className={styles.subtitle}>{text.value}</h3>
             <Link className={styles.link} to={buttonLink.value}>
---
+++ src/modules/header/headerLink.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HeaderLink = ({ name, type, to }) => {
+  if (type === 'central') {
+    return (
+      <Link
+        to={to}
+        className="flex items-center justify-center h2 tc dim pointer no-underline black f4 w-60">
+        <b>{name}</b>
+      </Link>
+    );
+  }
+
+  return (
+    <Link
+      to={to}
+      className="flex items-center justify-center h2 tc dim pointer no-underline black f7 w-10">
+      {name}
+    </Link>
+  );
+};
+export default HeaderLink;
